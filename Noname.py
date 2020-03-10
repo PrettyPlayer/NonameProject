5304b8d29bdeced2ce54ab90509a873fda39bd70
zlidenskardanom <zlidenskardanom@yandex.ru>
2020-03-10 14:18:18 +0300

    Save2

--- Noname.py
+++ Noname.py
@@ -8,6 +8,13 @@ class InitGame():
 		self.HEIGHT = 1080
 		self.fullscreen = 0
 		self.visiblemouse = True
+		self.rungame = True
+		
+		self.centeredwindow()
+		self.preinitsound()
+		self.initwindow()
+		self.setvisisblemouse()
+		self.managegame = ManageGame()
 	def centeredwindow(self):
 		os.environ['SDL_VIDEO_CENTERED'] = '1'
 	def preinitsound(self):
@@ -23,12 +30,10 @@ class InitGame():
 	def setvisisblemouse(self):
 		pygame.mouse.set_visible(self.visiblemouse)
 	def initstart(self):
-		self.centeredwindow()
-		self.preinitsound()
-		self.initwindow()
-		self.setvisisblemouse()
-		self.menuwindow = Window(60)
-		self.menuwindow.start()
+		while self.rungame:
+			self.managegame.update()
+		quit()
+		sys.exit()
 
 class Window():
 	def __init__(self, FPS):
@@ -36,14 +41,43 @@ class Window():
 		self.FPS = FPS
 	def start(self):
 		self.clock = pygame.time.Clock()
+		self.preinit()
 		while self.run:
-			for event in pygame.event.get():
-				if event.type == pygame.QUIT:
-					quit()
-			print('hello')
+			self.postinit()
+			self.exit()
 			pygame.display.update()
 			self.clock.tick(self.FPS)
+	def exit(self):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				thegame.managegame.scene = 0
+				self.run = False
+
+class MenuWindow(Window):
+	def preinit(self):
+		pass
+	def postinit(self):
+		pass
+
+class GameWindow(Window):
+	def preinit(self):
+		pass
+	def postinit(self):
+		pass
+
+class ManageGame():
+	def __init__(self):
+		self.scene = 1
+		self.gamewindow = GameWindow(60)
+		self.menuwindow = MenuWindow(60)
+	def update(self):
+		if self.scene == 0:
+			thegame.rungame = False
+		elif self.scene == 1:
+			self.menuwindow.start()
+		elif self.scene == 2:
+			self.gamewindow.start()
 
 if __name__ == "__main__":
-	theGame = InitGame()
-	theGame.initstart()
+	thegame = InitGame()
+	thegame.initstart()
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<h1><center>Рабочий файл - Noname.py</center></h1><h3><center>Потраченное время на проект:</center><center><pre>Дней	0
-Часов	9
-Минут	0
-Секунды	3</pre></center></h3>
+<h1><center>Рабочий файл - Noname.py</center></h1><h3><center>Потраченное время на проект:</center><center><pre>﻿Дней	0
+Часов	11
+Минут	16
+Секунды	7</pre></center></h3>
