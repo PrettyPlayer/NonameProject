d1b8af20ba60f50d41c6aaeda245a253636ce582
zlidenskardanom <zlidenskardanom@yandex.ru>
2020-03-12 00:03:40 +0300

    Добавлена музыка, фон главного меню, дождь, приложение слегка оптимизировано

--- Noname.py
+++ Noname.py
@@ -13,14 +13,13 @@ class InitGame():
 		self.preinitsound()
 		self.initwindow()
 		self.setvisisblemouse()
-		self.managegame = ManageGame()
 	def centeredwindow(self):
 		os.environ['SDL_VIDEO_CENTERED'] = '1'
 	def preinitsound(self):
 		pygame.mixer.pre_init(44100, -16, 2, 512)
 	def initwindow(self):
 		pygame.init()
-		pygame.display.set_icon(pygame.image.load(os.path.realpath("img\\2.png")))
+		pygame.display.set_icon(pygame.image.load(os.path.realpath("img\\papich\\2.png")))
 		if self.fullscreen == 0:
 			self.surf_main = pygame.display.set_mode((width, height))
 		elif self.fullscreen == 1:
@@ -28,15 +27,16 @@ class InitGame():
 		pygame.display.set_caption("Noname")
 	def setvisisblemouse(self):
 		pygame.mouse.set_visible(self.visiblemouse)
-	def initstart(self):
+	def initstart(self, copy):
+		self.managegame = ManageGame(copy)
 		while self.rungame:
 			self.managegame.update()
 
 class ManageGame():
-	def __init__(self):
+	def __init__(self, copy):
 		self.scene = 0
-		self.gamewindow = GameWindow()
-		self.menuwindow = MenuWindow()
+		self.gamewindow = GameWindow(copy)
+		self.menuwindow = MenuWindow(copy)
 	def update(self):
 		if self.scene == 0:
 			self.menuwindow.start(60)
@@ -45,4 +45,4 @@ class ManageGame():
 
 if __name__ == "__main__":
 	thegame = InitGame()
-	thegame.initstart()
+	thegame.initstart(thegame)
