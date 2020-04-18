f5328d5de377b14b3a778a7b4cc592222d0e7dcc
zlidenskardanom <zlidenskardanom@yandex.ru>
2020-04-18 23:24:30 +0300

    save

--- game/window/GameWindow.py
+++ game/window/GameWindow.py
@@ -24,80 +24,81 @@ class GameWindow(Window):
 			if event.type == pygame.QUIT:
 				self.exit()
 			elif event.type == pygame.KEYDOWN:
-				if event.key in (pygame.K_LEFT, pygame.K_DOWN, pygame.K_RIGHT):
-					self.papich.changePos(event.key)
+				if event.key == pygame.K_UP:
+					if self.numSfx < 2:
+						self.numSfx += 1
+						self.flagSfx = 1
+				if event.key == pygame.K_DOWN:
+					if 1 < self.numSfx:
+						self.numSfx -= 1
+						self.flagSfx = 1
 				if event.key == pygame.K_RETURN:
 					self.changeFullscreen()
 				if event.key == pygame.K_q:
-					self.snd1.play()
+					self.snd[1].play()
 				if event.key == pygame.K_a:
-					self.snd2.play()
+					self.snd[2].play()
 				if event.key == pygame.K_w:
-					self.snd3.play()
+					self.snd[3].play()
 				if event.key == pygame.K_s:
-					self.snd4.play()
+					self.snd[4].play()
 				if event.key == pygame.K_d:
-					self.snd5.play()
+					self.snd[5].play()
 				if event.key == pygame.K_r:
-					self.snd6.play()
+					self.snd[6].play()
 				if event.key == pygame.K_f:
-					self.snd7.play()
+					self.snd[7].play()
 				if event.key == pygame.K_t:
-					self.snd8.play()
+					self.snd[8].play()
 				if event.key == pygame.K_g:
-					self.snd9.play()
+					self.snd[9].play()
 				if event.key == pygame.K_h:
-					self.snd10.play()
+					self.snd[10].play()
 				if event.key == pygame.K_u:
-					self.snd11.play()
+					self.snd[11].play()
 				if event.key == pygame.K_j:
-					self.snd12.play()
+					self.snd[12].play()
 				if event.key == pygame.K_i:
-					self.snd13.play()
+					self.snd[13].play()
 				if event.key == pygame.K_k:
-					self.snd14.play()
+					self.snd[14].play()
 				if event.key == pygame.K_o:
-					self.snd15.play()
+					self.snd[15].play()
 				if event.key == pygame.K_l:
-					self.snd16.play()
+					self.snd[16].play()
 				if event.key == pygame.K_SEMICOLON:
-					self.snd17.play()
+					self.snd[17].play()
 				if event.key == pygame.K_LEFTBRACKET:
-					self.snd18.play()
+					self.snd[18].play()
 				if event.key == pygame.K_QUOTE:
-					self.snd19.play()
+					self.snd[19].play()
 				if event.key == pygame.K_RIGHTBRACKET:
-					self.snd20.play()
-	
+					self.snd[20].play()
 	def preInit(self):
 		pygame.mixer.music.stop()
-		self.backgroundMenuImage = Image()
-		self.backgroundMenuImage.createStaticImage(960, 540, "center",
-		                    "backgroundgameimage", "backgroundgame\\")
-		self.papich = Papich()
-		self.papich.createPapich(960, 380, 1/4, 1/4)
-		self.sfx = "fork"
-		self.snd1 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\1.wav")
-		self.snd2 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\2.wav")
-		self.snd3 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\3.wav")
-		self.snd4 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\4.wav")
-		self.snd5 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\5.wav")
-		self.snd6 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\6.wav")
-		self.snd7 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\7.wav")
-		self.snd8 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\8.wav")
-		self.snd9 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\9.wav")
-		self.snd10 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\10.wav")
-		self.snd11 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\11.wav")
-		self.snd12 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\12.wav")
-		self.snd13 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\13.wav")
-		self.snd14 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\14.wav")
-		self.snd15 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\15.wav")
-		self.snd16 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\16.wav")
-		self.snd17 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\17.wav")
-		self.snd18 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\18.wav")
-		self.snd19 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\19.wav")
-		self.snd20 = pygame.mixer.Sound("sfx\\" + self.sfx + "\\20.wav")
+		self.backgroundGameImage = Image()
+		self.backgroundGameImage.createStaticImage(960, 540, "center", "backgroundgameimage", "backgroundgame\\")
+		self.pianoImage = Image()
+		self.pianoImage.createStaticImage(960, 540, "center", "Piano", "backgroundgame\\")
+		self.numSfx = 1
+		self.flagSfx = 1
+		self.snd = {}
+		self.text = {}
+		self.startPosText = 530
+		self.changePosText = 0
+		for i in range(1, 12):
+			self.text[i] = Text()
+			self.text[i].createStaticText(str(i), "times", 24, COLOR.BLACK, self.startPosText + self.changePosText, 700)
+			self.changePosText += 86
 	def postInit(self):
-		self.backgroundMenuImage.showStaticImage()
-		self.papich.showPapich()
-	
+		if self.flagSfx:
+			if self.numSfx == 1:
+				self.sfx = "fork"
+			if self.numSfx == 2:
+				self.sfx = "oh"
+			for i in range(1, 21):
+				self.snd[i] = pygame.mixer.Sound("sfx\\" + self.sfx + "\\" + str(i) + ".wav")
+		self.backgroundGameImage.showStaticImage()
+		self.pianoImage.showStaticImage()
+		for i in range(1, 12):
+			self.text[i].showStaticText()
